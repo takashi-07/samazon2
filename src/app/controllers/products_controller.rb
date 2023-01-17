08e58cf7644178f27b7530b2eb7a3bd549df91cb
takashi-07 <ibn5100t@gmail.com>
2023-01-17 15:23:52 +0900

    create_favorite2

--- src/app/controllers/products_controller.rb
+++ src/app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_action :set_product, only: [:show, :edit, :update, :destroy, :favorite]
 
   def index
     @products = Product.all
@@ -35,12 +35,17 @@ class ProductsController < ApplicationController
     redirect_to products_url
   end
 
+  def favorite
+    current_user.toggle_like!(@product)
+    redirect_to product_url @product
+  end
+
   private
 
   def set_product
     @product = Product.find(params[:id])
   end
-  
+
   def product_params
     params.require(:product).permit(:name, :description, :price, :category_id)
   end
