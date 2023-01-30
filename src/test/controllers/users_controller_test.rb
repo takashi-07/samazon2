40daeb90420ecb7d242f256a0b983a3582d58340
takashi-07 <ibn5100t@gmail.com>
2023-01-30 15:06:50 +0900

    deproy ready

--- src/test/controllers/users_controller_test.rb
+++ src/test/controllers/users_controller_test.rb
@@ -2,22 +2,21 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "the truth" do
-    assert false
+    assert true
   end
 
-#  test "should get edit" do
- #  get users_edit_url
- #   assert_response :success
- # end
+  test "should get edit" do
+   get users_edit_url
+    assert_response :success
+  end
 
-# test "should get update" do
- #   get users_update_url
- #   assert_response :success
- # end
+ test "should get update" do
+    get users_update_url
+    assert_response :success
+  end
 
- # test "should get mypage" do
-  #  get users_mypage_url
-  #  assert_response :success
-  #end
-
+  test "should get mypage" do
+    get users_mypage_url
+    assert_response :success
+  end
 end
