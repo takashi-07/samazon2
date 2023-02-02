f415a92ba79eae09edeb04191c5430d4e0c69684
takashi-07 <ibn5100t@gmail.com>
2023-02-02 19:20:44 +0900

    deproy ready 2

--- src/config/environments/production.rb
+++ src/config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
 
   Rails.application.configure do
     # default url
-    config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+    config.action_mailer.default_url_options = { host: "https://samazon7.herokuapp.com" }
     # mail setting
     config.action_mailer.raise_delivery_errors = true
     config.action_mailer.delivery_method = :smtp
